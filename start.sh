3418b0c913ca68030601a95a5b9016ee486051f0
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-02-14 00:53:01 +0100

    start.sh: parametric about initramfs.cpio, which is renamed in .gz
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # (c) 2026, Roberto A. Foglietta <roberto.foglietta@gmail.com>, MIT license
 
-cmd="qemu-system-x86_64 -kernel bzImage -initrd initramfs.cpio -nographic \
-    -no-reboot -append 'root=/dev/ram0 rdinit=/init console=ttyS0 net.ifnames=0'\
-    -enable-kvm -cpu host -machine accel=kvm -boot order=dc -name tinylnx"
 append_for_kernel_debug="earlyprintk=serial nokaslr -pidfile vm.pid -panic=1"
-sh -c "$cmd $@"
-stty sane
+
+rfsimg="${1:-initramfs.cpio}"; test -r ${rfimg}.gz && rfsimg="${rfsimg}.gz"
+cmd="qemu-system-x86_64 -kernel bzImage -initrd ${rfsimg} -nographic -no-reboot\
+    -enable-kvm -cpu host -machine accel=kvm -boot order=dc -name tinylnx\
+    -append 'root=/dev/ram0 rdinit=/init console=ttyS0 net.ifnames=0'"\
+
+sh -c "$cmd $@"; stty sane

d1d397f68a082e4ea3284ee7c96f42b82a14dac6
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-02-14 00:27:12 +0100

    start.sh: uses predictable network interfaces name + debug
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 # (c) 2026, Roberto A. Foglietta <roberto.foglietta@gmail.com>, MIT license
 
 cmd="qemu-system-x86_64 -kernel bzImage -initrd initramfs.cpio -nographic \
-    -no-reboot -append 'root=/dev/ram0 rdinit=/init console=ttyS0 panic=1'\
+    -no-reboot -append 'root=/dev/ram0 rdinit=/init console=ttyS0 net.ifnames=0'\
     -enable-kvm -cpu host -machine accel=kvm -boot order=dc -name tinylnx"
+append_for_kernel_debug="earlyprintk=serial nokaslr -pidfile vm.pid -panic=1"
 sh -c "$cmd $@"
 stty sane

0bc03d30e1fde1c7ed232730f12ce773cb2de59d
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-02-14 00:00:14 +0100

    start.sh: include kvm native cpu accelaration
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 # (c) 2026, Roberto A. Foglietta <roberto.foglietta@gmail.com>, MIT license
 
 cmd="qemu-system-x86_64 -kernel bzImage -initrd initramfs.cpio -nographic \
-    -no-reboot -append 'root=/dev/ram0 rdinit=/init console=ttyS0 panic=1'"
+    -no-reboot -append 'root=/dev/ram0 rdinit=/init console=ttyS0 panic=1'\
+    -enable-kvm -cpu host -machine accel=kvm -boot order=dc -name tinylnx"
 sh -c "$cmd $@"
 stty sane

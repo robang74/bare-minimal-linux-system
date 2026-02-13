d2dec7f0b2022419c99a118268331b270b189de6
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-02-13 22:03:56 +0100

    workspace_linux.7z: deleted in favor of bzImage and initramfs.cpio
    Rationale: these files are binaries and can be compressed by a very
    snall amout, therefore there is no reason to store into .z archive.
    Moreover, the initramfs.cpio contains almost only busybox in terms
    of size and once compressed its size changes a little while in plain
    cpio format, it is esier to modify, easier to diff for git and faster
    to load when the kernel boot becuase skip decompressing it completelu.
    
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# (c) 2026, Roberto A. Foglietta <roberto.foglietta@gmail.com>, MIT license
+
+cmd="qemu-system-x86_64 -kernel bzImage -initrd initramfs.cpio -nographic \
+    -no-reboot -append 'root=/dev/ram0 rdinit=/init console=ttyS0 panic=1'"
+sh -c "$cmd"
+stty sane

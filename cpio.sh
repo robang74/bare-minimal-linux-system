676db7d78c08a45e6fefa053082f3573cc15f2c4
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-02-15 09:41:30 +0100

    cpio.sh: archive reproducibility and bugfix
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- cpio.sh
+++ cpio.sh
@@ -7,17 +7,23 @@ tmpdir=${3:-cpio.tmp}
 
 zcmd="gzip"; which pigz >/dev/null && zcmd="pigz"
 
-if [ "x$action" = "x-e" ]; then
-    mkdir -p $tmpdir
-    zcat $cpiofl | cpio -idmv -D $tmpdir
-elif [ "x$action" = "x-c" ]; then
-    rm -f $cpiofl
-    cd $tmpdir
-    find . | cpio -o -H newc | $zcmd -c > ../$cpiofl
-    cd - >/dev/null
-    du -ks $cpiofl | sed -e "s/\t/ Kb /"
-else
-    echo
-    echo "Usage: cpio.sh -e|-c file dir"
-    echo
-fi
+while true; do
+  if [ "x$action" = "x-e" ]; then
+      mkdir -p $tmpdir
+      zcat $cpiofl | cpio -idmv -D $tmpdir
+  elif [ "x$action" = "x-c" ]; then
+      rm -f $cpiofl
+      test -d $tmpdir || break
+      cd $tmpdir
+      chmod -c +x init bin/sh
+      find . -mindepth 1 -printf "%P\n" | cpio -o -H newc \
+        --reproducible --owner 0:0 | $zcmd -nc > ../$cpiofl
+      cd - >/dev/null
+      du -ks $cpiofl | sed -e "s/\t/ Kb /"
+  else
+      echo
+      echo "Usage: cpio.sh -e|-c file dir"
+      echo
+  fi
+  break
+done

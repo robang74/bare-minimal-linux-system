1b76ce92bbc1ac91ed2a7cf64cef1065a666cd54
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-03-05 12:53:01 +0100

    start.sh: even more predictable qemu VM than before
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- start.sh
+++ start.sh
@@ -111,11 +111,13 @@ else
 
   cmdlnx="$cmdlnx deferred_probe_timeout=0 page_alloc.shuffle=0 memtest=0"
   cmdlnx="lpj=2000000 noapic nolapic clocksource=pit video=off nomodeset $cmdlnx"
+  cmdlnx="$cmdlnx random.trust_cpu=off"
 
-  $qemubin -m ${QMSZE:-128M} -kernel ${kimg} -initrd ${rfsimg} -nographic \
-      -no-reboot -boot order=dc ${boxnme:-} -append "$cmdlnx" \
-          -nodefaults -serial mon:stdio -accel tcg -icount shift=0 \
-          -vga none -display none
+  $qemubin -m ${QMSZE:-128M} -kernel ${kimg} -initrd ${rfsimg} -nographic     \
+           -no-reboot -boot order=dc ${boxnme:-} -append "$cmdlnx ${KARGS:-}" \
+           -nodefaults -serial mon:stdio -vga none -display none -net none    \
+           -accel tcg -cpu qemu64 -smp 1 -icount shift=0,sleep=off,align=off  \
+           -rtc base=2026-03-01,clock=vm
   exit $?
 fi
 
